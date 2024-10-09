8122e7a1ad2d374d4e91e641d948045f8108654e
Rory_McG <rorymcgowan31@gmail.com>
2024-10-09 16:00:16 +0100

    Task 8

--- task8.py
+++ task8.py
@@ -1,2 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
+
+x = np.linspace(0, 10, 101)
+print(x)
+
+sin_wave=np.load("task7_sin.npy")
+cos_wave=np.load("task7_cos.npy")
+plt.plot(x,sin_wave)
+plt.plot(x,cos_wave)
+plt.legend(['Sin(X)','Cos(X)'])
+plt.show()
